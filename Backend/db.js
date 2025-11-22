5c21b8c0a784388f311453fea3471e97bb4e591f
Breno Henrique <brenoruiz2004@gmail.com>
2025-11-22 20:31:13 -0300

    feat(nuvem): adiciona o banco de dados na nuvem

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,13 @@
-const Pool = require('pg').Pool
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    password: "admin",
-    host: "localhost",
-    port: 5432,
-    database: "denuncias_golpes"
-})
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false
+});
 
-module.exports = pool
+module.exports = pool;
